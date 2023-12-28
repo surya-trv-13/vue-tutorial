32042f983462160f21224d2b0e93ab121c1f253f
Suryanarayan Rath <suryanarayanrath74@gmail.com>
2023-12-28 13:13:39 +0530

    feat:vue basics-databinding

---
+++ basics-01-starting-code/app.js
@@ -0,0 +1,22 @@
+const app = Vue.createApp({
+	data() {
+		// It can be added a Data which takes a method and always return a JS Object
+		return {
+			courseGoalA: "Learn Vue", // data can return anything required
+			courseGoalB: "Master Vue", // data can return anything required
+			vueLink: "https://vuejs.org/",
+		};
+	},
+	methods: {
+		outputGoal() {
+			const randomNumber = Math.random();
+			if (randomNumber < 0.5) {
+				return this.courseGoalA;
+			} else {
+				return this.courseGoalB;
+			}
+		},
+	},
+});
+
+app.mount("#user-goal"); // We can point to the attribute using CSS property for the mounting of Vue to be controlled by Vue
