96daaf5716900d47605b930f9005d7f8a9bb1fd1
SURYANARAYAN RATH <suryanarayanrath74@gmail.com>
2024-01-14 01:23:55 +0530

    Vue Behind the scene

--- behind-scenes-01-starting-setup/app.js
+++ behind-scenes-01-starting-setup/app.js
@@ -1,18 +1,33 @@
 const app = Vue.createApp({
-  data() {
-    return {
-      currentUserInput: '',
-      message: 'Vue is great!',
-    };
-  },
-  methods: {
-    saveInput(event) {
-      this.currentUserInput = event.target.value;
-    },
-    setText() {
-      this.message = this.currentUserInput;
-    },
-  },
+	data() {
+		return {
+			currentUserInput: "",
+			message: "Vue is great!",
+		};
+	},
+	methods: {
+		saveInput(event) {
+			this.currentUserInput = event.target.value;
+		},
+		setText() {
+			// this.message = this.currentUserInput;
+			this.message = this.$refs.userText.value; // The $ sign states that
+		},
+	},
 });
 
-app.mount('#app');
+app.mount("#app");
+
+// Vue bind uses JS proxies to handle the data sent to UI.
+
+// We can have more than one app for the vue, for example this.
+const app2 = Vue.createApp({
+	data() {
+		return {
+			messageVal: "Vue learning in app2",
+		};
+	},
+});
+
+app2.mount("#app2");
+
