913182d47c5d88bc165ad780ea64d21f48a9e779
SURYANARAYAN RATH <suryanarayanrath74@gmail.com>
2024-01-15 00:42:29 +0530

    Vue Components

---
+++ Component/cmp-intro-01-starting-setup/app.js
@@ -0,0 +1,56 @@
+const { createApp } = Vue;
+
+const app = createApp({
+	data() {
+		return {
+			detailsVisible: false,
+			friends: [
+				{
+					id: "surya",
+					name: "Suryanarayan Rath",
+					phone: "8897583610",
+					email: "rathsurya13@gmail.com",
+				},
+				{
+					id: "ipsita",
+					name: "Ipsita Sahoo",
+					phone: "8888988888",
+					email: "ipsitasahoo@gmail.com",
+				},
+			],
+		};
+	},
+});
+
+// Component can be created
+app.component("friend-component", {
+	template: `
+        <li>
+        <h2>{{friend.name}}</h2>
+        <button @click="toggleDetails">
+            {{detailsVisible ? 'Hide': 'Show'}} Details
+        </button>
+        <ul v-if="detailsVisible">
+            <li><strong>Phone:</strong> {{friend.phone}}</li>
+            <li><strong>Email:</strong> {{friend.email}}</li>
+        </ul>
+    </li>`, // Adding the template for the component
+	data() {
+		return {
+			detailsVisible: false,
+			friend: {
+				id: "surya",
+				name: "Suryanarayan Rath",
+				phone: "8897583610",
+				email: "rathsurya13@gmail.com",
+			},
+		};
+	},
+	methods: {
+		toggleDetails() {
+			this.detailsVisible = !this.detailsVisible;
+		},
+	},
+});
+
+app.mount("#app");
